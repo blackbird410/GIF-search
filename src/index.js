355b410937401546d966e8341a39944b950b4bed
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-02-21 05:30:49 +0800

    Code refactoring

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import './style.css';
+import "./style.css";
 
-const apiKey = 'j7tOBnFk5xoG5Jzt7Vu11RD5pMrd5AxL';
+const apiKey = "j7tOBnFk5xoG5Jzt7Vu11RD5pMrd5AxL";
 
 const displayContent = () => {
-  const main = document.createElement('div');
-  const img = document.createElement('img');
-  const search = document.createElement('input');
-  const fetchBtn = document.createElement('div');
+  const main = document.createElement("div");
+  const img = document.createElement("img");
+  const search = document.createElement("input");
+  const fetchBtn = document.createElement("div");
 
-  main.className = 'main';
-  search.placeholder = 'image here...'
-  fetchBtn.className = 'btn';
-  fetchBtn.textContent = 'Search image';
-  fetchBtn.addEventListener('click', fetchImage);
+  main.className = "main";
+  search.placeholder = "image here...";
+  fetchBtn.className = "btn";
+  fetchBtn.textContent = "Search image";
+  fetchBtn.addEventListener("click", fetchImage);
 
   main.appendChild(img);
   main.appendChild(search);
@@ -21,18 +21,20 @@ const displayContent = () => {
 };
 
 const fetchImage = () => {
-  const img = document.querySelector('img');
-  const search = document.querySelector('input');
+  const img = document.querySelector("img");
+  const search = document.querySelector("input");
   if (img && search.value) {
-
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search.value}`, {mode: 'cors'})
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search.value}`,
+      { mode: "cors" },
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
         img.src = response.data.images.original.url;
       })
-    .catch((error) => {
+      .catch((error) => {
         console.log(error);
       });
   }
